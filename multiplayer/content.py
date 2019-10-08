131e7f55f7641b656f16be9f80cf6b39575253f9
hemaolin <h01768@oa.cn-uniview.com>
2019-10-08 21:03:25 +0800

    modify host scan,because of UDP has no response

--- multiplayer/content.py
+++ multiplayer/content.py
@@ -10,7 +10,8 @@ import json
 from random import randint
 import information
 
-PORT = 80
+PORT = 8988
+
 
 class Node():
     def __init__(self, label):
@@ -47,6 +48,7 @@ class Node():
     def get_children_label(self):
         return [i.label for i in self.children]
 
+
 class Sock():
     def __init__(self):
         self.port = PORT
@@ -59,8 +61,8 @@ class Sock():
         self.done = False
 
         # MSG QUEUE
-        self.q = Queue.Queue()  # GET
-        self.q_send = Queue.Queue()
+        self.q = Queue.Queue()  # GET  [((info,msg), ip), (), ...]
+        self.q_send = Queue.Queue()  # SEND [((info,msg), ip), (), ...]
 
         # UDP sending
         thread_send = threading.Thread(target=self.msg_send)
@@ -73,27 +75,31 @@ class Sock():
         thread1.start()
 
     def msg_send(self):
-        """self.q_send是普通数据对象，传输的时候会加json.dumps"""
+        """
+        self.q_send是普通数据对象，传输的时候会加json.dumps
+        注： 消息队列不包含port，port都集成在这里
+        """
         while not self.done:
             if not self.q_send.empty():
-                msg,dest = self.q_send.get()
-                self.sock.sendto(json.dumps(msg), dest)
-                print('SEND [%s]:%s' % (str(dest), json.dumps(msg)))
+                msg, ip = self.q_send.get()
+                self.sock.sendto(json.dumps(msg), (ip, self.port))
+                print('SEND [%s]:%s' % (ip+str(self.port), json.dumps(msg)))
 
     def msg_recv(self):
+        """注： 消息队列不包含port，port在这里直接剔除了"""
         while not self.done:
             data, address = self.sock.recvfrom(1024)  # data=JSON, address=(ip, port)
-            self.q.put((json.loads(data),address[0]))  # 获取数据，将数据转换为正常数据，并且只提取ip，不提取port
+            self.q.put((json.loads(data), address[0]))  # 获取数据，将数据转换为正常数据，并且只提取ip，不提取port
 
     def localip(self):
         return socket.gethostbyname(socket.gethostname())
 
-    def scan_hostip(self):
+    def scan_hostip_tcp(self):
         delay = 0.001
         time_start = time.time()
-        ip_up =[]
+        ip_up = []
         ip_head = '.'.join(self.localip().split('.')[0:3])
-        ip_list = [ip_head+'.'+str(i) for i in range(256)]
+        ip_list = [ip_head + '.' + str(i) for i in range(256)]
         port_list = [self.port]
         for ip in ip_list:
             # print('Scan %s' % ip)
@@ -119,6 +125,28 @@ class Sock():
         # return [self.localip()]
         return ip_up
 
+    def scan_hostip(self):
+        hostip_list = []
+        other_msg = []
+        while not self.q.empty():
+            (info, msg), ip = self.q.get()
+            if info == 'host created':
+                hostip_list.append(ip)  # 记录ip地址，port就不记录了
+            else:
+                other_msg.append(((info, msg), ip))
+        for i in other_msg:  # 将剩余非建主机的消息放回队列
+            self.q.put(i)
+        return hostip_list
+
+    def broadcast(self, messages, ip_list):
+        for i in ip_list:
+            self.q_send.put((messages, i))
+
+    def host_broadcast(self):
+        ip_head = '.'.join(self.localip().split('.')[0:3])
+        ip_list = [ip_head + '.' + str(i) for i in range(256)]
+        self.broadcast(messages=('host created',''), ip_list=ip_list)
+
 
 class Widget():
     def __init__(self):
@@ -141,10 +169,10 @@ class Widget():
         self.sock = Sock()
         self.localip = self.get_localip()
 
-        self.msg_player = {'location':(randint(20,80)/100.0,randint(20,80)/100.0),
-                           'Plane':'F35',
-                           'Gun':200, 'Rocket':10, 'Cobra':3,}
-        self.dict_player = {self.localip:self.msg_player}
+        self.msg_player = {'location': (randint(20, 80) / 100.0, randint(20, 80) / 100.0),
+                           'Plane': 'F35',
+                           'Gun': 200, 'Rocket': 10, 'Cobra': 3, }
+        self.dict_player = {self.localip: self.msg_player}
 
         self.bool_create = False
         self.bool_join_enter = False
@@ -160,9 +188,12 @@ class Widget():
                                 'Gun':200, 'Rocket':10, 'Cobra':3}，
                         }
         """
+        # 局域网同网段群发
+        self.sock.host_broadcast()
+
         if not self.bool_create:
             node.add(Node('Host IP:' + self.localip))
-            self.sock.q_send.put((('player join',self.msg_player), (self.localip, self.sock.port)))  # 先给自己发个消息，自己就是主机
+            self.sock.q_send.put((('player join', self.msg_player), self.localip))  # 先给自己发个消息，自己就是主机
             self.bool_create = True
 
             start_node = Node('start')
@@ -170,20 +201,20 @@ class Widget():
             start_node.target = self.start_func
             # to be con...
         while not self.sock.q.empty():
-            (info, msg),ip = self.sock.q.get()  # 接受处理单个玩家的加入消息msg_player
-            if info=='player join' and ip not in node.get_children_label():
+            (info, msg), ip = self.sock.q.get()  # 接受处理单个玩家的加入消息msg_player
+            if info == 'player join' and ip not in node.get_children_label():
                 node.add(Node(ip))
                 self.dict_player[ip] = msg
                 for i in self.dict_player.keys():  # 给所有ip都发送所有玩家信息self.dict_player
                     if i != self.localip:  # 自己是主机，就不用发自己了
-                        self.sock.q_send.put((('dict_player',self.dict_player), (self.localip, self.sock.port)))
+                        self.sock.q_send.put((('dict_player', self.dict_player), self.localip))
             # 处理收到玩家退出消息，删除玩家
             # self.dict_player.pop(ip)
 
     def create_back_func(self, node):
         self.bool_create = False
         for i in node.children:
-            del(i)
+            del (i)
         node.children = []
 
     # JOIN FUNCTION
@@ -192,7 +223,7 @@ class Widget():
 
     def join_func(self, node):
         for i in node.children:
-            del(i)
+            del (i)
         node.children = []
         for ip in self.scan_hostip():
             ip_node = Node(ip)
@@ -205,10 +236,10 @@ class Widget():
     # JOIN_ENTER FUNCTION
     def join_enter(self, (node, host_ip)):
         if not self.bool_join_enter:
-            self.sock.q_send.put((('player join', self.msg_player), (host_ip, self.sock.port)))  # 先给主机发个加入
+            self.sock.q_send.put((('player join', self.msg_player), host_ip))  # 先给主机发个加入
             self.bool_join_enter = True
-        if 'Host:'+host_ip not in node.get_children_label():
-            node.add(Node('Host:'+host_ip))
+        if 'Host:' + host_ip not in node.get_children_label():
+            node.add(Node('Host:' + host_ip))
         # 获取&更新 self.dict_player
         while not self.sock.q.empty():
             (info, dict_player), ip = self.sock.q.get()
@@ -237,7 +268,7 @@ class Widget():
 
     def nodetree_produce(self):
         """
-        'Homing Missle':
+        'Homing Missile':
             'Local IP'
             'Create Game':
                 'host_ip'
@@ -278,7 +309,6 @@ class Widget():
 
         return start_node
 
-
     def main_loop(self):
         # 生成目录树
         self.root_node = self.nodetree_produce()
@@ -296,7 +326,7 @@ class Widget():
         while not self.done:
             self.frame += 1
             # 每过1s就进行一次当前节点的子项刷新
-            if self.frame%self.fps == 0:
+            if self.frame % self.fps == 0:
                 self.node_point.be_chosen()
                 self.list_node = self.node_point.children
             # 正常显示
@@ -307,7 +337,7 @@ class Widget():
             self.clock.tick(self.fps)
             if self.has_chosen:
                 # 如果所选的这个有子集，进入这个子集
-                if not self.has_backspace: #and
+                if not self.has_backspace:  # and
                     self.node_point = self.list_node[self.beginning_select_index]
                     self.node_point.be_chosen()
                     if self.node_point.children:  # 有子集就替换root指向
